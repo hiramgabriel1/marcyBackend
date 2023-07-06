f480aa1889bc5b3c85ea0d78da22ed0bfd2cd83b
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-07-06 02:35:40 -0700

    delete API

--- API/routes/auth/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { accesUser } from "../../controllers/auth/auth.js";
-
-const path = "/api/auth/access"
-const routerAccess = Router()
-
-routerAccess.post(path, accesUser)
-
-export default routerAccess
--- API/routes/auth/register.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser } from "../../controllers/auth/register.js";
-
-const path = "/api/auth/create"
-const routerCreate = Router()
-
-routerCreate.post(path, createUser)
-
-export default routerCreate
